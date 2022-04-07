83814734cde538d0314d42824847d6d16744381d
Ashur Cabrera <hello@ashur.cab>
2022-04-06 21:42:33 -0600

    Register plugin

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const {transform} = require("./src/plugin");
+
+/**
+ * @param {Object} pluginOptions
+ * @param {string} [pluginOptions.className] - optional class(es) to set on the resulting element
+ */
+module.exports = (eleventyConfig, pluginOptions = {}) =>
+{
+	eleventyConfig.addTransform( "emojiShortcodes", async function( content )
+	{
+		if( this.outputPath.endsWith( ".html" ) )
+		{
+			return await transform( content, pluginOptions );
+		}
+
+		return content;
+	});
+};
